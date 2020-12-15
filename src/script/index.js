534d80b834eb65c4840fc106d44aac3726b54090
ProgrammerMao-001 <mhf.work@qq.com>
2020-12-15 10:48:40 +0800

    12.15做了主页

---
+++ src/script/index.js
@@ -0,0 +1,15 @@
+// // 这个是index首页顶部的吸顶效果
+// const $helpNav = $('.help-nav');
+// $(window).on('scroll', () => {
+//     let $scrolltop = $(window).scrollTop(); //获取滚动条top值
+//     if ($scrolltop >= 100) {
+//         $helpNav.stop(true).animate({
+//             top: 0
+//         })
+//     } else {
+//         $helpNav.stop(true).animate({
+//             top: 130
+//         })
+//     }
+
+// });
--- src/script/index_module.js
+++ src/script/index_module.js
@@ -1,55 +1,31 @@
-define([], () => {
+define(['jlazyload'], () => {
     return {
         init: function() {
-            //代码实现
-            //1.鼠标移入左侧的li元素，显示右侧的大盒子。
-            const $list = $('.menu li');
-            const $cartlist = $('.cartlist');
-            const $items = $('.item');
-            $list.hover(function() {
-                $cartlist.show();
-                $(this).addClass('active').siblings('li').removeClass('active');
-                //切换内容发生改变，不同的li对应不同的内容块。
-                $items.eq($(this).index()).show().siblings('.item').hide();
-
-                //改变右侧的大盒子的位置
-                let $scrolltop = $(window).scrollTop();
-                let $bannertop = $('.banner').offset().top;
-                if ($scrolltop > $bannertop) {
-                    $cartlist.css({
-                        top: $scrolltop - $bannertop
+            //渲染+懒加载
+            const $list = $('.list123');
+            $.ajax({
+                url: 'http://localhost/dashboard/weipinhui/php/listdata.php',
+                dataType: 'json'
+            }).done(function(data) {
+                let $strhtml = '';
+                $.each(data, function(index, value) {
+                    $strhtml += `
+                        <li>
+ 
+                                <img class="lazy" data-original="${value.src}" width="200" height="200"/>
+                                <span>${value.title}</span>
+                                <p>￥${value.num}</p>
+                        </li>
+                    `;
+                });
+                $list.html($strhtml);
+                //渲染的下面进行懒加载操作
+                $(function() { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //显示方法：谈入
                     });
-                } else {
-                    $cartlist.css({
-                        top: 0
-                    });
-                }
-            }, function() {
-                $cartlist.hide();
+                });
             });
-
-            //2.鼠标移入右侧的大盒子，大盒子依然显示隐藏
-            $cartlist.hover(function() {
-                $(this).show();
-            }, function() {
-                $(this).hide();
-            });
-
-
-            //检测是否用户已经登录
-            if (localStorage.getItem('loginname')) {
-                $('.admin').show();
-                $('.login').hide();
-                $('.admin span').html(localStorage.getItem('loginname'));
-            }
-
-            //退出登录 - 删除本地存储
-            $('.admin a').on('click', function() {
-                $('.admin').hide();
-                $('.login').show();
-                localStorage.removeItem('loginname');
-            });
-
         }
     }
 });
